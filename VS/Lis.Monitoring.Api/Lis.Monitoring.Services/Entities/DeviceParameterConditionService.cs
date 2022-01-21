ac0e426aa9c3efee0e123cd4debe45546cb15831
Lendy <tomas.lendr@visionq.cz>
2022-01-21 15:33:48 +0100

    Update param, condition

--- VS/Lis.Monitoring.Api/Lis.Monitoring.Services/Entities/DeviceParameterConditionService.cs
+++ VS/Lis.Monitoring.Api/Lis.Monitoring.Services/Entities/DeviceParameterConditionService.cs
@@ -8,6 +8,7 @@ using Lis.Monitoring.Dto.Communication;
 using Lis.Monitoring.Infrastructure;
 using Lis.Monitoring.Services.Abstractions;
 using Lis.Monitoring.Services.Queries;
+using Microsoft.EntityFrameworkCore;
 
 namespace Lis.Monitoring.Services.Entities {
 	public class DeviceParameterConditionService : BaseEntityService<DeviceParameterCondition, long, DeviceParameterConditionQuery, DeviceParameterConditionDto>, IDeviceParameterConditionService {
@@ -16,6 +17,26 @@ namespace Lis.Monitoring.Services.Entities {
 
 		public override IQueryable<DeviceParameterCondition> EntityFilter(IQueryable<DeviceParameterCondition> query, DeviceParameterConditionQuery filtr) {
 			return query.Where(x => x.DeviceParameterId == filtr.DeviceParameterId);
+		}
+
+		/// <summary>
+		/// Provede změnu pro dané zařízení
+		/// </summary>
+		/// <param name="id">Identifikátor zařízení</param>
+		/// <param name="data">Data, která mají být změněná</param>
+		/// <returns></returns>
+		public override async Task<DeviceParameterCondition> UpdateAsync(long id, DeviceParameterConditionDto data) {
+			DeviceParameterCondition condition = await EntitySet.SingleOrDefaultAsync(u => u.Id == id);
+			if(condition == null) {
+				throw new Exception("Podmínka nebyla nalezena!");
+			}
+
+			condition.Operator = data.Operator;
+			condition.Value = data.Value;
+
+			await DbService.SaveChangesAsync();
+
+			return condition;
 		}
 	}
 }
